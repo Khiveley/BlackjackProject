7ef17a02772a5f1ebb2c82fd5a08fc0f1c12e5eb
Khiveley <kenneth.hiveley@gmail.com>
2021-08-15 11:40:03 -0500

    Implemented Dealer Hit or Stay method.

--- src/com/skilldistillery/blackjack/app/BlackjackApp.java
+++ src/com/skilldistillery/blackjack/app/BlackjackApp.java
@@ -19,11 +19,10 @@ public class BlackjackApp {
 		tableDealer.getNewDeck();
 		tableDealer.topCardDeal();
 		tableDealer.secondCardDeal();
-	
+//		tablePlayer.hitorstay
+		tableDealer.dealerHitOrStay();
 		
-		// I need a Dealer - hand and deck
-		// I need a Player - hand only
-		// Determine the rules of the game.
+	
 //	System.out.println("\u2665 This should be a Hearts suit symbol.");
 //    System.out.println("\u2666 This should be a Diamonds suit symbol.");
 //    System.out.println("\u2663 This should be a Clubs suit symbol.");
--- src/com/skilldistillery/blackjack/entities/Dealer.java
+++ src/com/skilldistillery/blackjack/entities/Dealer.java
@@ -1,58 +1,85 @@
 package com.skilldistillery.blackjack.entities;
 
+import java.util.Scanner;
 import java.util.List;
 
-public class Dealer extends Player{
-//Dealer responsibilities are to shuffle, 
-//deal a card up to a player
-//deal card down to self
-//deal card up to player
-//deal card up to self
+public class Dealer extends Player {
+
 //ask player to hit or stay
 //compare values of dealer cards against player cards
 //reshuffle
-	
+
 //private Dealer dealer1;
-private Player player1;
-private Deck gameDeck;
-private Hand dealerHand;
-private Hand player1Hand;
-private List<Card> usedCards;
+	private Player player1;
+	private Deck gameDeck;
+	private BlackjackHand dealerHand;
+	private BlackjackHand player1Hand;
+	private List<Card> usedCards;
 
-public Dealer(Player player1) {
+	public Dealer(Player player1) {
 //	this.dealer1 = new Dealer();
-	this.player1 = player1;
-	this.player1Hand = player1.getPlayerBlackjackHand();
-	this.gameDeck = new Deck();
-	this.dealerHand = new BlackjackHand();
-}
+		this.player1 = player1;
+		this.player1Hand = player1.getPlayerBlackjackHand();
+		this.gameDeck = new Deck();
+		this.dealerHand = new BlackjackHand();
+	}
 
-public void getNewDeck() {
-	gameDeck.shuffle();
-}
+	public void getNewDeck() {
+		gameDeck.shuffle();
+	}
 
-public void topCardDeal() {
-	Card card = gameDeck.dealCard();
-	System.out.println("The dealer has dealt you a: " + card);
-	player1Hand.addCard(card);
-	dealerHand.addCard(gameDeck.dealCard());
-}
-public void secondCardDeal() {
-	Card card = gameDeck.dealCard();
-	System.out.println("The dealer has dealt you a: " + card);
-	player1Hand.addCard(card);
-	System.out.println(player1Hand.getHandValue());
-	dealerHand.addCard(gameDeck.dealCard());
-	System.out.println("Dealer has " + this.dealerHand.getHand().get(1) + " showing. ");
-	
-}
-//public boolean hitOrStay() {
-//	
-//}
+	public void topCardDeal() {
+		Card card = gameDeck.dealCard();
+		System.out.println("The dealer has dealt you a: " + card);
+		player1Hand.addCard(card);
+		dealerHand.addCard(gameDeck.dealCard());
+	}
 
-@Override
-public String toString() {
-	return ("Dealer has" + dealerHand.getHand());
-}
+	public void secondCardDeal() {
+		Card card = gameDeck.dealCard();
+		System.out.println("The dealer has dealt you a: " + card);
+		player1Hand.addCard(card);
+		System.out.println(player1Hand.getHandValue());
+		dealerHand.addCard(gameDeck.dealCard());
+		System.out.println("Dealer has " + this.dealerHand.getHand().get(1) + " showing. ");
+
+	}
+//	public void playerHitOrStay() {
+//		boolean KeepGoing = true;
+//		for (Card card : player1Hand.getHand())
+//			System.out.println(card);
+//		while(keepGoing) {
+			
+//		}
+//	}
+
+	public void dealerHitOrStay() {
+		boolean keepGoing = true;
+		for (Card card : dealerHand.getHand())
+			System.out.println(card);
+		while (keepGoing) {
+			if (dealerHand.getHandValue() < 17) {
+				Card card = gameDeck.dealCard();
+				dealerHand.addCard(card);
+				System.out.println("The Dealers has to hit. He dealt a " + card);
+				System.out.println("The Dealer's total is :" + dealerHand.getHandValue());
+			}
+			else if (dealerHand.isBust()){
+				System.out.println("The Dealer's total is :" + dealerHand.getHandValue());
+				System.out.println("The dealer busts!  You win!");
+				keepGoing = false;
+			}
+			else {
+				keepGoing = false;
+				System.out.println("The Dealer's total is :" + dealerHand.getHandValue());
+				System.out.println("The Dealer stays.");
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return ("Dealer has" + dealerHand.getHand());
+	}
 
 }
